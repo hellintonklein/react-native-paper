fdcdd9722c0f5b420da3fed6b18f9ec5cac4be58
Hellinton Eduardo Klein <hellintonklein@hotmail.com>
2019-05-09 11:39:05 -0300

    Chore: 57639 - Accordion - ajuste demo usage
    removido duplicidade de implementação do código de demonstração de uso do componente, conforme CR.

--- src/components/List/ListAccordion.js
+++ src/components/List/ListAccordion.js
@@ -90,14 +90,6 @@ type State = {
  *     return (
  *       <List.Section title="Accordions">
  *         <List.Accordion
- *           title="Uncontrolled Accordion"
- *           left={props => <List.Icon {...props} icon="folder" />}
- *         >
- *           <List.Item title="First item" />
- *           <List.Item title="Second item" />
- *         </List.Accordion>
- *
- *         <List.Accordion
  *           title="Controlled Accordion"
  *           left={props => <List.Icon {...props} icon="folder" />}
  *           expanded={this.state.expanded}
